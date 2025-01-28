b69d29e598fe33aa6cf964b4bdf6b8ece95d653e
Will Brown <will.brown.gd@gmail.com>
2025-01-28 10:32:28 -0500

    Push

--- project/src/player/Player.gd
+++ project/src/player/Player.gd
@@ -1,3 +1,21 @@
 extends Spatial
 
-var _isMoving = false;
+var _isMoving := false
+onready var _pivot := $Pivot
+onready var _dice := $Pivot/Dice
+
+func _process(delta):
+	if (!_isMoving):
+		roll(Vector3.FORWARD)
+	
+func roll(dir):
+	# Move Pivot
+	_pivot.translate(dir)
+	_dice.translate(-dir)
+	
+	# Animate
+	
+	# Update position
+	transform.origin += dir * 2
+	_pivot.transform = Transform.IDENTITY
+	_dice.transform = Transform.IDENTITY
